1dccb425cb9cf42038f14e31735eb29753747a96
Jerry Lan <1577628623@qq.com>
2018-09-18 19:31:14 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# node-bank.js
+
+> Get bank name by bank card no. 
+>
+> 根据银行卡卡号获取银行名称
+
+## 安装
+
+```
+npm i @ionepub/node-bank --save
+```
+
+## 使用方法
+
+```js
+var Bank = require('@ionepub/node-bank');
+var bank_card_no = '6222600260001072444'; // 银行卡卡号，可以是不完整的，如622260
+var bank_name = Bank.get(bank_card_no);
+console.log(bank_name); // 交通银行
+```
+
---
+++ index.js
@@ -0,0 +1,67 @@
+/*!
+ * node-bank v1.0.0
+ * https://github.com/ionepub/node-bank
+ *
+ * Licensed MIT © ionepub
+ */
+'use strict';
+var Bank;
+(function(owner){
+	/**
+	 * 银行卡卡号前缀数字
+	 */
+	var prefix = '';
+
+	/**
+	 * 银行卡数据列表
+	 */
+	var _bank_list = require("fs").readFileSync(__dirname+"/res.json"); // __dirname 当前文件所在文件夹
+	_bank_list = JSON.parse(_bank_list);
+
+	/**
+	 * 银行卡号正则
+	 */
+	var reg = /[0-9]{2,10}/;
+
+	/**
+	 * 检查获取银行名称
+	 * @param {string} bank_code 银行卡卡号
+	 * @param {int}    length    前缀长度
+	 * @return {string|false}    未找到时返回false，否则返回银行名称
+	 */
+	var _check = function(bank_code, length){
+		length = parseInt(length);
+		if(length < 2 || length > 10){
+			return false;
+		}
+		prefix = bank_code.substr(0, length);
+
+		if(typeof _bank_list[prefix] === 'string'){
+			return _bank_list[prefix];
+		}
+
+		return false;
+	}
+
+	/**
+	 * 获取银行名称主方法
+	 * @param {string} bank_code 银行卡卡号
+	 * @return {string}    未找到时返回空字符串，否则返回银行名称
+	 */
+	owner.get = function(bank_code){
+		if(!reg.test(bank_code)){
+			return '';
+		}
+		for (var i = 10; i >= 2; i--) {
+			var result = _check(bank_code, i);
+			if(result){
+				return result;
+				break;
+			}
+		}
+		return '';
+	};
+
+})(Bank = {});
+
+module.exports = Bank;
