e944d181eeb57fb3172bbc38f0cd8b71eac1f39b
Jerry Lan <1577628623@qq.com>
2018-09-19 11:32:26 +0800

    增加银行名称列表资源文件；增加获取银行列表方法

--- index.js
+++ index.js
@@ -15,13 +15,19 @@ var Bank;
 	/**
 	 * 银行卡数据列表
 	 */
-	var _bank_list = require("fs").readFileSync(__dirname+"/res.json"); // __dirname 当前文件所在文件夹
+	var fs = require("fs");
+	var _bank_list = fs.readFileSync(__dirname+"/res.json"); // __dirname 当前文件所在文件夹
 	_bank_list = JSON.parse(_bank_list);
 
 	/**
 	 * 银行卡号正则
 	 */
 	var reg = /[0-9]{2,10}/;
+
+	/**
+	 * 银行名称列表
+	 */
+	var _bank_name_arr = [];
 
 	/**
 	 * 检查获取银行名称
@@ -61,6 +67,18 @@ var Bank;
 		}
 		return '';
 	};
+
+	/**
+	 * 获取银行名称列表
+	 * @return {object} 银行名称数组
+	 */
+	owner.list = function(){
+		if( _bank_name_arr.length == 0 ){
+			var tmp = fs.readFileSync(__dirname+"/list.json"); // __dirname 当前文件所在文件夹
+			_bank_name_arr = JSON.parse(tmp);
+		}
+		return _bank_name_arr;
+	}
 
 })(Bank = {});
 
